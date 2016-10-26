3510e4edd1c1646813bc00c8719c1bd297bbf015
Abdelhakim Akodadi <akodadi.abdelhakim@gmail.com>
2016-10-26 23:24:28 +0100

    Use post collection deserializer in service get posts

--- service/service.c
+++ service/service.c
@@ -6,83 +6,18 @@
 #include "service.h"
 
 post_collection service_get_posts() {
-    string err_msg;
     long http_code;
     string raw_result;
     post_collection result;
-    unsigned long post_i;
-    json_t *json_root, *json_post, *json_id, *json_body, *json_created_at, *json_updated_at;
-    json_error_t error;
     raw_result = repository_get(TARGET_POSTS, &http_code);
-
-    json_root = json_loads(raw_result.s, 0, &error);
-
-    if (!json_root) {
-        err_msg = string_createf("Cannot parse json content \n\"%s\".\nError on line %d column %d : %s",
-                raw_result.s,
-                error.line,
-                error.column,
-                error.text);
-        error_handle(JSON_DECODE_ERROR, 0, err_msg.s);
-    }
-
-    if (!json_is_array(json_root)) {
-        json_decref(json_root);
-        err_msg = string_createf("Cannot parse json content \n\"%s\".\nRoot element is not a json array", raw_result.s);
-        error_handle(JSON_DECODE_ERROR, 0, err_msg.s);
-    }
-
-    result.len = json_array_size(json_root);
-    result.p = malloc(result.len * sizeof (post));
-    for (post_i = 0; post_i < result.len; post_i++) {
-        json_post = json_array_get(json_root, post_i);
-
-        if (!json_is_object(json_post)) {
-            json_decref(json_root);
-            err_msg = string_createf("Cannot parse json content \n\"%s\".\nElements in root array are not json objects", raw_result.s);
-            error_handle(JSON_DECODE_ERROR, 0, err_msg.s);
-        }
-
-        json_id = json_object_get(json_post, "id");
-        json_body = json_object_get(json_post, "body");
-        json_created_at = json_object_get(json_post, "created_at");
-        json_updated_at = json_object_get(json_post, "updated_at");
-
-        if (!json_is_integer(json_id)) {
-            json_decref(json_root);
-            err_msg = string_createf("Cannot parse json content \n\"%s\".\n id element is not a integer", raw_result.s);
-            error_handle(JSON_DECODE_ERROR, 0, err_msg.s);
-        }
-
-        if (!json_is_string(json_body)) {
-            json_decref(json_root);
-            err_msg = string_createf("Cannot parse json content \n\"%s\".\n body element is not a string", raw_result.s);
-            error_handle(JSON_DECODE_ERROR, 0, err_msg.s);
-        }
-
-        if (!json_is_integer(json_created_at)) {
-            json_decref(json_root);
-            err_msg = string_createf("Cannot parse json content \n\"%s\".\n created_at element is not a integer", raw_result.s);
-            error_handle(JSON_DECODE_ERROR, 0, err_msg.s);
-        }
-
-        if (!json_is_integer(json_updated_at)) {
-            json_decref(json_root);
-            err_msg = string_createf("Cannot parse json content \n\"%s\".\n updated_at element is not a integer", raw_result.s);
-            error_handle(JSON_DECODE_ERROR, 0, err_msg.s);
-        }
-
-        result.p[post_i].id = json_integer_value(json_id);
-        result.p[post_i].body = string_create(json_string_value(json_body));
-        result.p[post_i].t.created_at = json_integer_value(json_created_at);
-        result.p[post_i].t.updated_at = json_integer_value(json_updated_at);
-    }
-
+    
+    post_collection_initialize(&result);
+    
+    result = post_collection_deserialize(raw_result);
+    
     string_destroy(&raw_result);
-    json_decref(json_root);
 
     return result;
-
 }
 
 post service_get_post(unsigned long id) {
